1203f28fb97fc561c8d98074f86e19bf25e7381c
Maria Agudelo <112421879+Marietagu@users.noreply.github.com>
2024-10-19 16:43:42 +1300

    Update backend files

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel,conlist
 from typing import List,Optional
 import pandas as pd
@@ -9,6 +10,13 @@ dataset=pd.read_csv('Data/dataset.csv',compression='gzip')
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
 
 class params(BaseModel):
     n_neighbors:int=5
